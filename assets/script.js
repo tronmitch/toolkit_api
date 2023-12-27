28ea23820858f1fb58ce9fa613586dbd8f8d5749
Mitchell <tmitchell@cdsvisual.com>
2023-12-27 16:36:04 -0700

    latest

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,23 @@ const commnadCardElements = $('#command-card')
 fetch(json).then((response)=> response.json()).then(data => {
     // console.log(data)
     console.log(data.find(obj=>{
-        console.log(obj['Command'])
-        const commandTitle = obj['Command'].charAt(0).toUpperCase() + obj['Command'].slice(1)
-        const description = obj['ShortDescription']
-        const example = obj['example']
-        commandListElements.append(`<li id="${obj['Command']}">${obj['Command']}</li>`)
-        // commnadCardElements.append(`<section class="cards">\n
-        //                                 <h2>${commandTitle}</h2>\n 
-        //                                 <p class="description">${description}</p>\n
-        //                                 <p class="syntax">${example}</p>\n
-        //                             </section>`)
+        console.log(obj['syntax'])
+        const command = obj['Command']
+        const commandTitle = command.charAt(0).toUpperCase() + obj['Command'].slice(1)
+        const shortDescription =  obj['ShortDescription'].toLowerCase()
+        const longDexcription = obj['LongDesciption']
+        const syntax = obj['syntax']
+        const example =obj['example']
+        commandListElements.append(`<a href="#${command}"><li>${command}</li></a>`)
+        commnadCardElements.append(`<section id=${command} class="cards">
+                                        <div class="title-description">
+                                            <h2>${commandTitle}</h2><p> - ${shortDescription} </p>
+                                        </div><br>\n
+                                        <p class="description">${longDexcription}</p><br>
+                                        <p class="syntax">syntax: ${syntax}</p>
+                                        <p class="example">example: ${example}</p>
+                                    </section>`)
 
     }))       
 })
-
 
